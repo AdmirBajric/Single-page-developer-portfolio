b097157ae2cdb70a669c0b096d3753c33e461a77
Admir Bajric <admir.bajric.dev@gmail.com>
2023-01-25 00:20:22 +0100

    Update files

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const submit = document.querySelector("#submit");
-const userInput = document.querySelector("#name");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const messageInput = document.querySelector("#message");
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(v8n().empty().test(userInput.value));
-  console.log(v8n().minLength(2).test([userInput.value]));
+  console.log(v8n().empty().test(nameInput.value));
+  console.log(v8n().empty().test(emailInput.value));
+  console.log(v8n().empty().test(messageInput.value));
 });
