1147d364d051931d264ee7ab6d84e6497d8befa4
Admir Bajric <admir.bajric.dev@gmail.com>
2023-01-25 22:00:40 +0100

    Update files

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 const submit = document.querySelector("#submit");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const messageInput = document.querySelector("#message");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const err = document.querySelector(".error-name");
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(v8n().empty().test(nameInput.value));
-  console.log(v8n().empty().test(emailInput.value));
-  console.log(v8n().empty().test(messageInput.value));
+  test(name.value) ? error(name) : success(name);
+  test(message.value) ? error(message) : success(message);
 });
+
+const test = (value) => {
+  return v8n().empty().test(value);
+};
+
+const error = (value) => {
+  console.log(value.name);
+  const err = document.querySelector(`.error-${value.name}`);
+  value.style.borderBottom = `0.1rem solid var(--red)`;
+  err.style.opacity = 1;
+};
+
+const success = (value) => {
+  const err = document.querySelector(`.error-${value.name}`);
+  value.style.borderBottom = `0.1rem solid var(--neonGreen)`;
+  err.style.opacity = 0;
+};
